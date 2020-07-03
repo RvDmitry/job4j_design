4c72b13d74ec4324d707ce71ba590f12e650a232
RvDmitry <rvdmitry@gmail.com>
2020-07-03 07:40:44 +0600

    Тестирование класса User.

--- chapter_001/src/test/java/ru/job4j/collection/UserTest.java
+++ chapter_001/src/test/java/ru/job4j/collection/UserTest.java
@@ -16,11 +16,31 @@ public class UserTest {
 
     @Test
     public void whenUser() {
-        GregorianCalendar birthday = new GregorianCalendar(2000, 02, 10);
+        GregorianCalendar birthday = new GregorianCalendar(2000, 2, 10);
         User user = new User("Ivan", birthday);
         user.setChildren(1);
         assertThat(user.getName(), is("Ivan"));
         assertThat(user.getChildren(), is(1));
         assertThat(user.getBirthday(), is(birthday));
+    }
+
+    @Test
+    public void whenUserEquals() {
+        GregorianCalendar birthday = new GregorianCalendar(2000, 2, 10);
+        User user1 = new User("Ivan", birthday);
+        user1.setChildren(1);
+        User user2 = new User("Ivan", birthday);
+        user2.setChildren(1);
+        assertThat(user1, is(user2));
+    }
+
+    @Test
+    public void whenUserNoEquals() {
+        GregorianCalendar birthday = new GregorianCalendar(2000, 2, 10);
+        User user1 = new User("Ivan", birthday);
+        user1.setChildren(1);
+        User user2 = new User("Petr", birthday);
+        user2.setChildren(1);
+        assertNotEquals(user1, user2);
     }
 }
