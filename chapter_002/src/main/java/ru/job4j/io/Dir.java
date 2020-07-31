dd1b76bbe8cafccbc555ac5694de7d3615114b89
RvDmitry <rvdmitry@gmail.com>
2020-07-31 23:29:22 +0600

    4.0. File [#305622]

---
+++ chapter_002/src/main/java/ru/job4j/io/Dir.java
@@ -0,0 +1,50 @@
+package ru.job4j.io;
+
+import java.io.File;
+
+/**
+ * Class Dir
+ * Класс выводит список каталогов в папке projects.
+ * @author Dmitry Razumov
+ * @version 1
+ */
+public class Dir {
+    /**
+     * Главный метод программы. Выводит содержимое заданного каталога.
+     * @param args Параметры командной строки
+     */
+    public static void main(String[] args) {
+        File file = new File("c:\\projects");
+        if (!file.exists()) {
+            throw new IllegalArgumentException(
+                    String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(
+                    String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+        System.out.println(String.format("size : %s", file.getTotalSpace()));
+        for (File subfile : file.listFiles()) {
+            System.out.println(subfile.getName() + ": " + getFolderSize(subfile));
+        }
+    }
+
+    /**
+     * Метод вычисляет размер каталога с файлами.
+     * @param folder Каталог, размер которого нужно вычислить
+     * @return Размер каталога
+     */
+    private static long getFolderSize(File folder) {
+        long length = 0;
+        File[] files = folder.listFiles();
+        int count = files.length;
+        for (int i = 0; i < count; i++) {
+            if (files[i].isFile()) {
+                length += files[i].length();
+            } else {
+                length += getFolderSize(files[i]);
+            }
+        }
+        return length;
+    }
+}
