b9e54bffe923200275a89c630134dce9c3fe7660
RvDmitry <rvdmitry@gmail.com>
2020-09-23 15:44:44 +0600

    SQL [#224183]

--- chapter_003/src/main/resources/script.sql
+++ chapter_003/src/main/resources/script.sql
@@ -2,13 +2,20 @@ create table users (id serial primary key, name varchar(20) unique not null);
 
 create table meeting (id serial primary key, name varchar (20) unique not null);
 
-create table statuses (id serial primary key,
+create table statuses (
+id serial primary key,
 user_id int REFERENCES users (id) not null,
-meeting_id int REFERENCES meeting (id) not null);
+meeting_id int REFERENCES meeting (id) not null,
+status boolean not null
+);
 
 select m.name, count(m.name) from statuses s
 join meeting m on s.meeting_id = m.id
-group by m.name;
+where s.status = 'y' group by m.name;
 
-select m.name from meeting m left
-join statuses s on m.id = s.meeting_id where s.meeting_id is null;
+select distinct m.name from meeting m left
+join statuses s on m.id = s.meeting_id where s.meeting_id is null
+or s.meeting_id in (
+select meeting_id from statuses where status = 'n'
+except
+select meeting_id from statuses where status = 'y');
