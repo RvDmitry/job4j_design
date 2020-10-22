879af6017804145055a25c0e046819b0e8a1ce8b
RvDmitry <rvdmitry@gmail.com>
2020-10-22 23:59:50 +0600

    2. Парковка машин [#305654]

--- chapter_005/src/main/java/ru/job4j/design/lsp/parking/Car.java
+++ chapter_005/src/main/java/ru/job4j/design/lsp/parking/Car.java
@@ -1,5 +1,7 @@
 package ru.job4j.design.lsp.parking;
 
+import java.util.Objects;
+
 /**
  * Class Car
  * Класс описывает автомобили.
@@ -7,8 +9,64 @@ package ru.job4j.design.lsp.parking;
  * @version 1
  */
 public class Car  {
+    /**
+     * Номер машины.
+     */
+    private final String id;
+    /**
+     * Размерность машины. Определяет, является ли машина легковой для n = 1 или грузовой для n > 1.
+     */
+    private final int n;
 
+    /**
+     * Конструктор инициализирует автомобиль.
+     * @param id Номер машины.
+     * @param n Размерность машины.
+     */
     public Car(String id, int n) {
+        this.id = id;
+        this.n = n;
+    }
 
+    /**
+     * Метод возвращает номер машины.
+     * @return Номер машины.
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * Метод возвращает размерность машины.
+     * @return Размерность машины.
+     */
+    public int getN() {
+        return n;
+    }
+
+    /**
+     * Метод сравнивает два автомобиля и определяет, один и тот же это автомобиль или разные машины.
+     * @param o Автомобиль.
+     * @return true, если автомобиль один и тот же, иначе false.
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Car car = (Car) o;
+        return Objects.equals(id, car.id);
+    }
+
+    /**
+     * Метод возвращает хеш-код машины.
+     * @return Хеш-код.
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
--- chapter_005/src/main/java/ru/job4j/design/lsp/parking/CarParking.java
+++ chapter_005/src/main/java/ru/job4j/design/lsp/parking/CarParking.java
@@ -1,5 +1,9 @@
 package ru.job4j.design.lsp.parking;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
 /**
  * Class CarParking
  * Класс описывает парковку машин.
@@ -7,28 +11,142 @@ package ru.job4j.design.lsp.parking;
  * @version 1
  */
 public class CarParking implements Parking {
+    /**
+     * Массив содержит машины на парковке для легковых автомобилей.
+     */
+    private final Car[] cars;
+    /**
+     * Массив содержит машины на парковке для грузовых автомобилей.
+     */
+    private final Car[] trucks;
 
+    /**
+     * Конструктор инициализирует парковку для машин.
+     * @param c Количество мест для легковых машин.
+     * @param t Количество мест для грузовых машин.
+     */
     public CarParking(int c, int t) {
-
+        cars = new Car[c];
+        trucks = new Car[t];
     }
 
+    /**
+     * Метод осуществляет парковку машины.
+     * @param car Автомобиль.
+     * @return true, если машина установлена на парковку, иначе false.
+     */
     @Override
     public boolean park(Car car) {
+        if (checkPark(car)) {
+            return false;
+        }
+        if (car.getN() == 1) {
+            for (int i = 0; i < cars.length; i++) {
+                if (cars[i] == null) {
+                    cars[i] = car;
+                    return true;
+                }
+            }
+        } else {
+            for (int i = 0; i < trucks.length; i++) {
+                if (trucks[i] == null) {
+                    trucks[i] = car;
+                    return true;
+                }
+            }
+            int check = 0;
+            for (int i = 0; i < cars.length; i++) {
+                if (cars[i] == null) {
+                    if (++check == car.getN()) {
+                        while (check-- > 0) {
+                            cars[i--] = car;
+                        }
+                        return true;
+                    }
+                } else {
+                    check = 0;
+                }
+            }
+        }
         return false;
     }
 
+    /**
+     * Метод осуществляет удаление машины с парковки.
+     * @param car Автомобиль.
+     * @return true, если машина убрана с парковки, иначе false.
+     */
     @Override
     public boolean unpark(Car car) {
+        if (car.getN() == 1) {
+            for (int i = 0; i < cars.length; i++) {
+                if (Objects.equals(cars[i], car)) {
+                    cars[i] = null;
+                    return true;
+                }
+            }
+        } else {
+            for (int i = 0; i < trucks.length; i++) {
+                if (Objects.equals(trucks[i], car)) {
+                    trucks[i] = null;
+                    return true;
+                }
+            }
+            for (int i = 0; i < cars.length; i++) {
+                if (Objects.equals(cars[i], car)) {
+                    int j = i + car.getN();
+                    while (i < j) {
+                        cars[i++] = null;
+                    }
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
+    /**
+     * Метод возвращает количество машин на парковке для легковых машин.
+     * @return Количество машин.
+     */
     @Override
     public int numberCars() {
-        return 0;
+        int i = 0;
+        int count = 0;
+        while (i < cars.length) {
+            if (cars[i] != null) {
+                count++;
+                i += cars[i].getN();
+            } else {
+                i++;
+            }
+        }
+        return count;
     }
 
+    /**
+     * Метод возвращает количество машин на парковке для грузовых машин.
+     * @return Количество машин.
+     */
     @Override
     public int numberTrucks() {
-        return 0;
+        int count = 0;
+        for (Car truck : trucks) {
+            if (truck != null) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    /**
+     * Метод проверяет, установлен ли на парковке автомобиль.
+     * @param car Машина.
+     * @return true, если машина есть на парковке, иначе false.
+     */
+    private boolean checkPark(Car car) {
+        List<Car> c = Arrays.asList(cars);
+        List<Car> t = Arrays.asList(trucks);
+        return c.contains(car) || t.contains(car);
     }
 }
