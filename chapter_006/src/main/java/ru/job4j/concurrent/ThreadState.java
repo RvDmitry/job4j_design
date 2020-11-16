a2728b22a96386308326810acfc4455f56473157
RvDmitry <rvdmitry@gmail.com>
2020-11-16 23:40:11 +0600

    1.1. Состояние нити. [#407150]

---
+++ chapter_006/src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -0,0 +1,31 @@
+package ru.job4j.concurrent;
+
+/**
+ * Class ThreadState
+ * В классе практикуется работа с состоянием потоков.
+ * @author Dmitry Razumov
+ * @version 1
+ */
+public class ThreadState {
+    /**
+     * Главный метод программы.
+     * @param args Параметры командной строки.
+     */
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> System.out.println("First Thread!")
+        );
+        Thread second = new Thread(
+                () -> System.out.println("Second Thread!")
+        );
+        System.out.println(first.getState());
+        System.out.println(second.getState());
+        first.start();
+        second.start();
+        while (first.getState() != Thread.State.TERMINATED
+                || second.getState() != Thread.State.TERMINATED) {
+            System.out.print("");
+        }
+        System.out.println("Работа завершена.");
+    }
+}
