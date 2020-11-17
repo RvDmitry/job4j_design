1f3048cae3f5cb8e06de71f380c0e76f1efc59c0
RvDmitry <rvdmitry@gmail.com>
2020-11-18 01:12:06 +0600

    3.1. Прерывание блокированной нити. [#407152]

--- chapter_006/src/main/java/ru/job4j/concurrent/Wget.java
+++ chapter_006/src/main/java/ru/job4j/concurrent/Wget.java
@@ -19,7 +19,7 @@ public class Wget {
                         try {
                             Thread.sleep(1000);
                         } catch (InterruptedException e) {
-                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
                         }
                     }
                 }
