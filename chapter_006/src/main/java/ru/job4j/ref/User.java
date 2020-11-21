efda2785f821b82b384102c576869a62df7e4c3c
RvDmitry <rvdmitry@gmail.com>
2020-11-21 11:44:57 +0600

    4. Thread без общих ресурсов [#407159]

---
+++ chapter_006/src/main/java/ru/job4j/ref/User.java
@@ -0,0 +1,61 @@
+package ru.job4j.ref;
+
+/**
+ * Class User
+ * Класс реализует пользователя.
+ * @author Dmitry Razumov
+ * @version 1
+ */
+public class User {
+    /**
+     * Идентификатор пользователя.
+     */
+    private int id;
+    /**
+     * Имя пользователя.
+     */
+    private String name;
+
+    /**
+     * Метод создает пользователя.
+     * @param name Имя пользователя.
+     * @return Пользователь.
+     */
+    public static User of(String name) {
+        User user = new User();
+        user.name = name;
+        return user;
+    }
+
+    /**
+     * Метод возвращает идентификатор пользователя.
+     * @return Идентификатор.
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Метод задает идентификатор пользователя.
+     * @param id Идентификатор.
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Метод возвращает имя пользователя.
+     * @return Имя.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Метод задает имя пользователя.
+     * @param name Имя.
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ chapter_006/src/main/java/ru/job4j/ref/UserCache.java
@@ -0,0 +1,55 @@
+package ru.job4j.ref;
+
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Class UserCache
+ * Класс реализует кеш пользователей.
+ * @author Dmitry Razumov
+ * @version 1
+ */
+@ThreadSafe
+public class UserCache {
+    /**
+     * Коллекция хранит пользователей.
+     */
+    private final ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<>();
+    /**
+     * Объект для генерации ключей коллекции.
+     */
+    private final AtomicInteger id = new AtomicInteger();
+
+    /**
+     * Метод добавляет пользователя в хранилище.
+     * @param user Пользователь.
+     */
+    public void add(User user) {
+        users.put(id.incrementAndGet(), User.of(user.getName()));
+    }
+
+    /**
+     * Метод ищет пользователя в хранилище по его идентификатору.
+     * @param id Идентификатор.
+     * @return Пользователь.
+     */
+    public User findById(int id) {
+        return User.of(users.get(id).getName());
+    }
+
+    /**
+     * Метод возвращает список пользователей содержащихся в хранилище.
+     * @return Список пользователей.
+     */
+    public List<User> findAll() {
+        List<User> list = new ArrayList<>();
+        for (User user : users.values()) {
+            list.add(User.of(user.getName()));
+        }
+        return list;
+    }
+}
