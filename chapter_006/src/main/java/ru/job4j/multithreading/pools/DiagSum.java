e10e7cbbd894c9d94ce7075cf5d5305e1f0a4dc3
RvDmitry <rvdmitry@gmail.com>
2020-12-08 21:23:56 +0600

    4. CompletableFuture [#407141]

--- chapter_006/src/main/java/ru/job4j/multithreading/pools/DiagSum.java
+++ chapter_006/src/main/java/ru/job4j/multithreading/pools/DiagSum.java
@@ -12,14 +12,19 @@ import java.util.concurrent.ExecutionException;
  * @version 1
  */
 public class DiagSum {
-
+    /**
+     * Метод асинхронно вычисляет сумму всех диагоналей матрицы.
+     * И результат возвращает ввиде массива сумм.
+     * @param matrix Матрица.
+     * @return Массив сумм диагоналей.
+     * @throws ExecutionException Исключение.
+     * @throws InterruptedException Исключение.
+     */
     public static int[] asyncSum(int[][] matrix) throws ExecutionException, InterruptedException {
         int n = matrix.length;
         int[] sums = new int[2 * n];
         Map<Integer, CompletableFuture<Integer>> futures = new HashMap<>();
-        // считаем сумму по главной диагонали
         futures.put(0, getTask(matrix, 0, n - 1, n - 1));
-        // считаем суммы по побочным диагоналям
         for (int k = 1; k <= n; k++) {
             futures.put(k, getTask(matrix, 0, k - 1,  k - 1));
             if (k < n) {
@@ -32,6 +37,14 @@ public class DiagSum {
         return sums;
     }
 
+    /**
+     * Метод вычисляет сумму чисел на заданной диагонали матрицы.
+     * @param data Матрица.
+     * @param startRow Строка, на которой начинается диагональ.
+     * @param endRow  Строка, на которой заканчивается диагональ.
+     * @param startCol Столбец, на котором начинается диагональ.
+     * @return Объект, который содержит сумму чисел на диагонали.
+     */
     public static CompletableFuture<Integer> getTask(int[][] data, int startRow,
                                                      int endRow, int startCol) {
         return CompletableFuture.supplyAsync(() -> {
